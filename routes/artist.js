7396e7962c72dcedc1b9086136c212432d514c32
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2018-07-10 20:30:48 +0200

    images working

--- routes/artist.js
+++ routes/artist.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const clientId = '456b82e403914c6f85d795481c50c909';
+const clientSecret = 'edfefe26f9a44cd19d13cdf1b7467ba3';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+spotifyApi.clientCredentialsGrant()
+  .then((data) => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, (err) => {
+    console.log('Something went wrong when retrieving an access token', err);
+  });
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   const artistName = req.query.name;
   spotifyApi.searchArtists(artistName)
     .then(data => {
+      console.log(data.body.artists.items.images);
       // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.render('artist', {artist: data.body.artists.items});
     })
     .catch(err => {
-      // ----> 'HERE WE CAPTURE THE ERROR'
+      console.log(err);
+      // throw err;
     });
-
-  res.render('artist', {artistName: artistName});
 });
 
 module.exports = router;
